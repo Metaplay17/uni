3f9526155558c4f416f91f0cea4221f1c41785d6
Maxim <danin35@yandex.ru>
2023-10-21 16:21:32 +0300

    21-10-2023

---
+++ Tasks3.py
@@ -0,0 +1,75 @@
+# NUMBER 1
+# a, b, c, d = (int(input()) for _ in range(4))
+# if a == 0 or b % a != 0:
+#     print("NO")
+# else:
+#     print(-1 * (b // a))
+
+
+# NUMBER 2
+# k, m, n = (int(input()) for _ in range(3))
+# if n % k == 0:
+#     print((n // k) * 2 * m)
+# else:
+#     print((n // k + 1) * 2 * m)
+
+
+# NUMBER 3
+# a1, b1, c1, a2, b2, c2, a, b, c = (int(input()) for _ in range(9))
+#
+# help_sum = a1 + b1 + c1
+# a_help = max(a1, b1, c1)
+# c_help = min(a1, b1, c1)
+# b_help = help_sum - a_help - c_help
+# (
+#     a1,
+#     b1,
+#     c1,
+# ) = (
+#     a_help,
+#     b_help,
+#     c_help,
+# )
+#
+# help_sum = a2 + b2 + c2
+# a_help = max(a2, b2, c2)
+# c_help = min(a2, b2, c2)
+# b_help = help_sum - a_help - c_help
+# (
+#     a2,
+#     b2,
+#     c2,
+# ) = (
+#     a_help,
+#     b_help,
+#     c_help,
+# )
+#
+# help_sum = a + b + c
+# a_help = max(a, b, c)
+# c_help = min(a, b, c)
+# b_help = help_sum - a_help - c_help
+# (
+#     a,
+#     b,
+#     c,
+# ) = (
+#     a_help,
+#     b_help,
+#     c_help,
+# )
+#
+# if (c > (c1 + c2) and b > (b1 + b2) and a > a1 and a > a2) or (
+#     c > c1 and c > c2 and b > b1 and b > b2 and a > (a1 + a2)
+# ):
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# NUMBER 4
+k = int(input())
+if k == 3 or (k >= 5 and k % 10 != 7):
+    print("YES")
+else:
+    print("NO")
---
+++ Tasks5.py
@@ -0,0 +1,156 @@
+from math import log10
+
+# NUMBER 1
+# n = int(input())
+# mx = float("-inf")
+# while n != 0:
+#     mx = max(n, mx)
+#     n = int(input())
+# print(mx)
+
+
+# NUMBER 2
+# n = int(input())
+# ans = 0
+# while n != 0:
+#     if n % 2 == 0:
+#         ans += 1
+#     n = int(input())
+# print(ans)
+
+
+# NUMBER 3
+# n = int(input())
+# ans = 0
+# last = n
+# while n != 0:
+#     if n > last:
+#         ans += 1
+#     last = n
+#     n = int(input())
+# print(ans)
+
+
+# NUMBER 4
+# n = int(input())
+# mx, pred_mx = float("-inf"), float("-inf")
+# while n != 0:
+#     if n > mx:
+#         pred_mx = mx
+#         mx = n
+#     elif n > pred_mx:
+#         pred_mx = n
+#     n = int(input())
+# print(pred_mx)
+
+
+# NUMBER 5
+# n = int(input())
+# mx = float("-inf")
+# count = 0
+# while n != 0:
+#     if n > mx:
+#         mx = n
+#         count = 1
+#     elif n == mx:
+#         count += 1
+#     n = int(input())
+# print(count)
+
+
+# NUMBER 6
+# a, b = int(input()), int(input())
+# while a > b:
+#     if a % 2 == 0 and a // 2 >= b:
+#         print("//2")
+#         a //= 2
+#     else:
+#         print("-1")
+#         a -= 1
+
+
+# NUMBER 7
+# n = int(input())
+# count_tens = int(log10(n))
+# ans = 0
+# while n > 10:
+#     ans += ((n % 10) * 10**count_tens)
+#     count_tens -= 1
+#     n //= 10
+# ans += n % 10
+# print(ans)
+
+
+# NUMBER 8
+# k = int(input())
+# ans = 0
+# while k > 10:
+#     clone = k
+#     count_nums = (int(log10(clone)) + 1) // 2
+#     ten_degree = 1
+#     while count_nums > 0:
+#         if not clone % (10**ten_degree) == clone // (10**count_nums):
+#             break
+#         count_nums -= 1
+#     else:
+#         ans += 1
+#     k -= 1
+#
+# print(ans + 9)
+
+
+# NUMBER 9
+# n = int(input())
+# k = 1
+# ans = 0
+# while n != 0:
+#     last = n
+#     n = int(input())
+#     if last == n:
+#         k += 1
+#         ans = max(ans, k)
+#     else:
+#         k = 1
+# print(ans)
+
+
+# NUMBER 10
+# n = int(input())
+# k_up, k_down = 1, 1
+# ans = 0
+# while n != 0:
+#     last = n
+#     n = int(input())
+#     if last == n:
+#         k_up, k_down = 1, 1
+#     elif last > n:
+#         k_up += 1
+#         k_down = 1
+#         ans = max(ans, k_up)
+#     else:
+#         k_down += 1
+#         k_up = 0
+#         ans = max(ans, k_down)
+# print(ans)
+
+# NUMBER 11
+# n = int(input())
+# ans = float("inf")
+# pred_mx, pot_mx, post_mx = float("inf"), float("inf"), float("inf")
+# last_mx = 0
+# count_max = 0
+# while n != 0:
+#     pred_mx = pot_mx
+#     pot_mx = post_mx
+#     post_mx = n
+#     if pred_mx < pot_mx and pot_mx > post_mx:
+#         ans = min(ans, last_mx)
+#         last_mx = 0
+#         count_max += 1
+#     last_mx += 1
+#     n = int(input())
+# if count_max < 2:
+#     print(0)
+# else:
+#     print(ans)
+
